e2b4b1021b8a39ca0bff726bdecd7fbfa0ab8bd4
AlejandroDopico2 <72970555+AlejandroDopico2@users.noreply.github.com>
2026-03-07 00:21:40 +0000

    Deploying to gh-pages from @ AlejandroDopico2/AlejandroDopico2.github.io@0568b8924e6a9aa07dd8ce1ba4a009cd3b85905b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,12 +52,12 @@ ninja.data = [{
           description: "I was honored to receive the award for Best Master’s Thesis during AEPIA’s AI Summer School (EVIA 2025) in Gijón.",
           section: "News",handler: () => {
               window.location.href = "/news/250711_evia_award/";
-            },},{id: "news-i-am-currently-on-a-research-stay-at-inesc-tec-university-of-porto-collaborating-with-joão-gama-university-of-porto-and-pedro-henriques-abreu-university-of-coimbra-collaboration-on-deep-learning-and-continual-learning-for-resource-efficient-settings",
-          title: 'I am currently on a research stay at INESC TEC (University of Porto),...',
-          description: "I am on a research stay at INESC TEC (Porto) with João Gama and Pedro Henriques Abreu.",
-          section: "News",},{id: "news-a-paper-on-federated-learning-has-been-accepted-at-esann-2026",
+            },},{id: "news-a-paper-on-federated-learning-has-been-accepted-at-esann-2026",
           title: 'A paper on federated learning has been accepted at ESANN 2026.',
           description: "",
+          section: "News",},{id: "news-i-am-currently-on-a-research-stay-at-inesc-tec-university-of-porto-collaborating-with-joão-gama-university-of-porto-and-pedro-henriques-abreu-university-of-coimbra-collaboration-on-deep-learning-and-continual-learning-for-resource-efficient-settings",
+          title: 'I am currently on a research stay at INESC TEC (University of Porto),...',
+          description: "I am on a research stay at INESC TEC (Porto) with João Gama and Pedro Henriques Abreu.",
           section: "News",},{id: "projects-artlens",
           title: 'ArtLens',
           description: "HackUDC 2025 Edition",
